d21c6ec62c9ab66ab6d8958d28f420bfeb34afdc
Florian Finke <flo@randomknowledge.org>
2013-01-07 13:55:37 +0100

    changed reserved word "type" to "s_type"

--- cactus/plugins/sass.py
+++ cactus/plugins/sass.py
@@ -12,24 +12,24 @@ class SassPlugin(CactusPluginBase):
         self.run(dist=True)
 
     def run(self, *args, **kwargs):
-        type = self.config.get("type", "sass")
-        if type != "sass" and type != "scss":
-            type = "sass"
+        s_type = self.config.get("type", "sass")
+        if s_type != "sass" and s_type != "scss":
+            s_type = "sass"
 
         dist = kwargs.get("dist", False)
         buildpath = "dist" if dist else "build"
-        sass_dir = os.path.join(self.site.paths['static'], type)
+        sass_dir = os.path.join(self.site.paths['static'], s_type)
         css_dir = os.path.join(self.site.paths[buildpath], 'static', 'css')
         if not os.path.isdir(sass_dir) or not os.listdir(sass_dir):
             return
 
         main_file_sass = self.config.get("main_file_sass", "main.sass")
-        if type == "scss":
+        if s_type == "scss":
             main_file_sass = self.config.get("main_file_scss", "main.scss")
         main_file_css = self.config.get("main_file_css", "main.css")
         sass = self.config.get(
             "command",
-            "%s -t compressed {input} {output}" % type
+            "%s -t compressed {input} {output}" % s_type
         )
 
         cmd = sass.format(
