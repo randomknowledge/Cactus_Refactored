1acebcd54af70db4a4e7e9cb2a9373317ae6445b
Florian Finke <flo@randomknowledge.org>
2013-02-28 18:48:02 +0100

    fixed minifyjs plugin in development mode

--- cactus/plugins/minifyjs.py
+++ cactus/plugins/minifyjs.py
@@ -2,12 +2,43 @@
 import logging
 import re
 import os
+import shutil
 from cactus.utils import fileList
 from cactus.plugin_base import CactusPluginBase
 from slimit import minify
 
 
 class MinifyJsPlugin(CactusPluginBase):
+    infile = None
+    outfile = None
+    buildpath = None
+    js_dir = None
+
+    def _prepare(self, *args, **kwargs):
+        dist = kwargs.get("dist", False)
+        self.buildpath = "dist" if dist else "build"
+        self.js_dir = os.path.join(self.site.paths[self.buildpath], 'static', 'js')
+        if not os.path.isdir(self.js_dir) or not os.listdir(self.js_dir):
+            return False
+
+        self.infile = os.path.abspath(
+            os.path.join(
+                self.js_dir,
+                'main.js'
+            )
+        )
+
+        self.outfile = os.path.abspath(
+            os.path.join(
+                self.js_dir,
+                self.config.get(
+                    'ouput_filename',
+                    'main.js'
+                )
+            )
+        )
+        return True
+
     def _minify(self, infile, outfile=None):
         if outfile is None:
             outfile = infile
@@ -20,37 +51,25 @@ class MinifyJsPlugin(CactusPluginBase):
         f.write(minify(content, mangle=False, mangle_toplevel=False))
         f.close()
 
-    def postDist(self, *args, **kwargs):
-        buildpath = "dist"
-        js_dir = os.path.join(self.site.paths[buildpath], 'static', 'js')
-        if not os.path.isdir(js_dir) or not os.listdir(js_dir):
+    def postBuild(self, *args, **kwargs):
+        if not self._prepare(dist=False):
             return
 
-        infile = os.path.abspath(
-            os.path.join(
-                js_dir,
-                'main.js'
-            )
-        )
+        # Do not minify in devlopment mode, only rename file
+        shutil.move(self.infile, self.outfile)
 
-        outfile = os.path.abspath(
-            os.path.join(
-                js_dir,
-                self.config.get(
-                    'ouput_filename',
-                    'main.js'
-                )
-            )
-        )
+    def postDist(self, *args, **kwargs):
+        if not self._prepare(dist=True):
+            return
 
-        self._minify(infile, outfile)
+        self._minify(self.infile, self.outfile)
 
-        if infile != outfile and not self.config.get('keep_unminified', True):
-            os.remove(infile)
+        if self.infile != self.outfile and not self.config.get('keep_unminified', True):
+            os.remove(self.infile)
 
         if self.config.get('minify_vendor_scripts', False):
-            for filename in fileList(js_dir):
-                if os.path.abspath(filename) != outfile and not re.search(r'\.min\.js$', filename, re.I):
+            for filename in fileList(self.js_dir):
+                if os.path.abspath(filename) != self.outfile and not re.search(r'\.min\.js$', filename, re.I):
                     self._minify(filename)
 
         logging.info("done.")
